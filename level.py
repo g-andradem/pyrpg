04d11627554c4517d93979da9d541aaed47432ef
guilherme <guilherme.anm10@gmail.com>
2026-09-10 10:46:46 -0300

    testing

--- level.py
+++ level.py
@@ -8,6 +8,7 @@ from settings import *
 from tile import Tile
 from player import Player
 from debug import debug
+from support import import_csv_layout
 
 class Level:
     def __init__(self):
@@ -21,16 +22,33 @@ class Level:
         self.create_map()
 
     def create_map(self):
-        for row_index, row in enumerate(WORLD_MAP):
-            for col_index, col in enumerate(row):
-                x = col_index * TILESIZE
-                y = row_index * TILESIZE
-                if col == 'p':
-                    self.player = Player(
-                        (x, y),[self.visible_sprites], self.obstacles_sprites
-                    )
-                if col == 'x':
-                    Tile((x, y),[self.visible_sprites, self.obstacles_sprites])
+        layouts = {
+            'bourdary': import_csv_layout('data/test/map.csv')
+        }
+
+        for style, layout in layout.items():
+            for row_index, row in enumerate(WORLD_MAP):
+                for col_index, col in enumerate(row):
+                    x = col_index * TILESIZE
+                    y = row_index * TILESIZE
+                    if style == 'bourdary':
+                        Tile(
+                            (x, y), 
+                            [self.visible_sprites, self.obstacles_sprites], 
+                            'invisible', 
+                            surface = pygame.Surface((TILESIZE, TILESIZE))
+                        )
+
+        # 
+        #         if col == 'p':
+        #             self.player = Player(
+        #                 (x, y),[self.visible_sprites], self.obstacles_sprites
+        #             )
+        #         if col == 'x':
+        #             Tile((x, y),[self.visible_sprites, self.obstacles_sprites])
+        self.player = Player(
+            (400, 300),[self.visible_sprites], self.obstacles_sprites
+        )
                 
     def run(self):
         #update and draw the game
@@ -47,12 +65,21 @@ class YSortCameraGroup(pygame.sprite.Group):
         self.half_height = self.display_surface.get_size()[1] // 2
         self.offset = pygame.math.Vector2(self.half_width, self.half_height)
 
+        # creating the floor
+        self.floor_surf = pygame.image.load('data/test/chao.png').convert()
+        self.floor_rect = self.floor_surf.get_rect(topleft = (0,0))
+
     def custom_draw(self, player):
 
         # getting the offset
         self.offset.x = player.rect.centerx - self.half_width
         self.offset.y = player.rect.centery - self.half_height
 
-        for sprite in self.sprites():
+        # drawing the floor
+        floor_offset_pos = self.floor_rect.topleft - self.offset
+        self.display_surface.blit(self.floor_surf, floor_offset_pos)
+
+        # for sprite in self.sprites():
+        for sprite in sorted(self.sprites(), key = lambda sprite: sprite.rect.centery):
             offset_pos = sprite.rect.topleft - self.offset
             self.display_surface.blit(sprite.image, offset_pos)
